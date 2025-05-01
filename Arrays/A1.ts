a0450c5cc028894f295bfa14e6d45f0d3a8e2843
codersunny812 <sengersunny448@gmail.com>
2025-05-01 11:33:46 +0530

    arrays in ts

---
+++ Arrays/A1.ts
@@ -0,0 +1,11 @@
+const arr = [2,4,5,6,9,10,1,-1];
+
+//function to traver the array 
+function traverseArr(arr:number[]){
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+}
+
+
+traverseArr(arr)
