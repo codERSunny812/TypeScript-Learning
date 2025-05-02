9ca28dbec9e34ad7522974e9dbe048427525548f
codersunny812 <sengersunny448@gmail.com>
2025-05-02 23:33:27 +0530

    enums in ts

---
+++ Enums/A.ts
@@ -0,0 +1,16 @@
+
+enum KeyInput {
+    "UP",
+    "Down",
+    "Left",
+    "Right"
+}
+
+
+function Direction(KeyPressed:KeyInput){
+
+    console.log("user has pressed",KeyInput);
+}
+
+Direction(KeyInput.Down)
+
