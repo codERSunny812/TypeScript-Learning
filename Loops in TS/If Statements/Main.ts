565c623993e6b211c34fddb12092a7019cf2fd1e
codersunny812 <sengersunny448@gmail.com>
2025-05-11 16:19:24 +0530

    loops in ts

---
+++ Loops in TS/If Statements/Main.ts
@@ -0,0 +1,14 @@
+let discount: number; //variable 
+let itemCount = 11;
+
+if (itemCount > 0 && itemCount <= 5) {
+  discount = 5; // 5% discount
+} else if (itemCount > 5 && itemCount <= 10) {
+  discount = 10; // 10% discount
+} else if (itemCount > 10) {
+  discount = 15; // 15%
+} else {
+  throw new Error('The number of items cannot be negative!');
+}
+
+console.log(`You got ${discount}% discount. `);
